a2d1c2c097f3a4fe08759e4564523b0e01757e10
ice-rogue <icerogue4@gmail.com>
2019-09-20 20:17:21 +0500

    Add contacts, upgraded view visit

--- app.rb
+++ app.rb
@@ -1,53 +1,39 @@
-require 'rubygems'
-require 'sinatra'
-require "sinatra/reloader"
-
-configure do
-  enable :sessions
-end
-
-helpers do
-  def username
-    session[:identity] ? session[:identity] : 'Hello stranger'
-  end
-end
-
-before '/secure/*' do
-  unless session[:identity]
-    session[:previous_url] = request.path
-    @error = 'Sorry, you need to be logged in to visit ' + request.path
-    halt erb(:login_form)
-  end
-end
+# require 'rubygems'
+# require 'sinatra'
+# require "sinatra/reloader"
 
 get '/' do
   erb 'Can you handle a <a href="/secure/place">secret</a>?'
 end
 
-get '/login/form' do
-  erb :login_form
-end
-
-post '/login/attempt' do
-  session[:identity] = params['username']
-  session[:identity] = params['password']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from
-end
-
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
-end
-
-get '/secure/place' do
-  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
-end
 
 get "/about" do
   erb :about
 end
 
+get "/visit" do
+  erb :visit
+end
+
+post "/visit" do
+  @username = params[:username]
+  @password = params[:password]
+  @dateandtime = params[:dateandtime]
+  @barber = params[:barber]
+
+  f = File.open("public/users.txt", "a")
+  f.write "Имя: #{@username}. Телефон: #{@phone}. Время и дата: #{@dateandtime}. Парикмахер: #{@barber}\n"
+  f.close
+  erb :visit
+end
+
+
+
+
 get "/contacts" do
+  erb :contacts
+end
+
+post "/contacts" do
   erb :contacts
 end
