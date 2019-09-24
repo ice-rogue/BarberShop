0e61af03271a35327b21cedbf2e630cdfa79eb04
ice-rogue <icerogue4@gmail.comr>
2019-09-24 20:03:50 +0500

    Global program update

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-# require 'rubygems'
-# require 'sinatra'
-# require "sinatra/reloader"
+require 'rubygems'
+require 'sinatra'
+require "sinatra/reloader"
 
 get '/' do
   erb 'Can you handle a <a href="/secure/place">secret</a>?'
@@ -17,9 +17,15 @@ end
 
 post "/visit" do
   @username = params[:username]
-  @password = params[:password]
+  @phone = params[:phone]
   @dateandtime = params[:dateandtime]
   @barber = params[:barber]
+
+  hh = {:username => "Введите имя", :phone => "Введите телефон", :dateandtime => "Введите дату и время"}
+  @error = hh.select {|key,_| params[key] == ""}.values.join("; ")
+  if @error != ""
+    return erb :visit
+  end
 
   f = File.open("public/users.txt", "a")
   f.write "Имя: #{@username}. Телефон: #{@phone}. Время и дата: #{@dateandtime}. Парикмахер: #{@barber}\n"
@@ -31,9 +37,5 @@ end
 
 
 get "/contacts" do
-  erb :contacts
-end
-
-post "/contacts" do
   erb :contacts
 end
