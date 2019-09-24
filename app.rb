f3e1ec8fc4f3e8d5141dacd15992b3ef178a08bc
ice-rogue <icerogue4@gmail.comr>
2019-09-24 21:15:42 +0500

    Save visit info to database

--- app.rb
+++ app.rb
@@ -3,9 +3,13 @@ require "sinatra"
 require "sinatra/reloader"
 require "sqlite3"
 
+def get_db
+  return SQLite3::Database.new "barbershop.db"
+end
+
 configure do
-  @db = SQLite3::Database.new "barbershop.db"
-  @db.execute "CREATE TABLE IF NOT EXISTS 
+  db = get_db
+  db.execute "CREATE TABLE IF NOT EXISTS 
     Users
     (
     'id' INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -41,15 +45,16 @@ post "/visit" do
     return erb :visit
   end
 
+  db = get_db
+  db.execute "INSERT INTO Users (username, phone, datestamp, barber) values (?,?,?,?)", [@username, @phone, @datetime, @barber]
+
   f = File.open("public/users.txt", "a")
   f.write "Имя: #{@username}. Телефон: #{@phone}. Время и дата: #{@dateandtime}. Парикмахер: #{@barber}\n"
   f.close
   erb :visit
 end
 
-
-
-
 get "/contacts" do
   erb :contacts
 end
+
